619066b35d7a7f9ed2becd35b3c5894823f63947
Ryan Duffy <ryan.duffy@lge.com>
2020-11-19 10:41:42 -0800

    fix app title
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Daily Schedule</title>
       </Head>
 
       <main>
